581348a12d4c4c567bd5c48e617ffba556f88d32
William Bai <williambai2001@gmail.com>
2015-10-23 09:43:17 +0800

    add plugin in projects.

--- examples/helloworld/www/js/index.js
+++ examples/helloworld/www/js/index.js
@@ -33,6 +33,71 @@ var app = {
     // The scope of 'this' is the event. In order to call the 'receivedEvent'
     // function, we must explicitly call 'app.receivedEvent(...);'
     onDeviceReady: function() {
+        document.addEventListener("pause", app.pauseEvent, false);
+        document.addEventListener("resume", app.resumeEvent, false);
+        document.addEventListener("online", app.onlineEvent, false);
+        document.addEventListener("offline", app.offlineEvent, false);
+        document.addEventListener("backbutton", app.backButtonEvent, false);
+        document.addEventListener("menubutton", app.menuButtonEvent, false);
+        document.addEventListener("searchbutton", app.searchButtonEvent, false);
+        document.addEventListener("startcallbutton", app.startcallButtonEvent, false);
+        document.addEventListener("endcallbutton", app.endcallButtonEvent, false);
+        document.addEventListener("volumedownbutton", app.volumeDownButtonEvent, false);
+        document.addEventListener("volumeupbutton", app.volumeUpButtonEvent, false);
+        document.addEventListener("batterystatus", app.batteryStatusEvent, false);
+        app.backgroundModePlugin();
+        app.receivedEvent('deviceready');
+    },
+    
+    pauseEvent: function(){
+        alert('pause, app go to background now.');
+    },
+    
+    resumeEvent: function(){
+        alert('resume, app go to front now.');
+    },
+
+    onlineEvent: function(){
+        alert('online');
+    },
+    
+    offlineEvent: function(){
+        alert('offline');
+    },
+
+    backButtonEvent: function(){
+        alert('back button clicked.');
+    },
+
+    menuButtonEvent: function(){
+        alert('menu button clicked.');
+    },
+
+    searchButtonEvent: function(){
+        alert('search button clicked.');
+    },
+
+    startcallButtonEvent: function(){
+        alert('startcall button clicked.');
+    },
+
+    endcallButtonEvent: function(){
+        alert('endcall button clicked.');
+    },
+
+    volumeDownButtonEvent: function(){
+        alert('volume down button clicked.');
+    },
+
+    volumeUpButtonEvent: function(){
+        alert('volume up button clicked.');
+    },
+
+    batteryStatusEvent:function(){
+        alert('buttery status changed by 1%.');
+    },
+
+    backgroundModePlugin: function(){
         // Android customization
         cordova.plugins.backgroundMode.setDefaults({ text:'Doing heavy tasks.'});
         // Enable background mode
@@ -47,7 +112,6 @@ var app = {
                 });
             }, 5000);
         }
-        app.receivedEvent('deviceready');
     },
     // Update DOM on a Received Event
     receivedEvent: function(id) {
